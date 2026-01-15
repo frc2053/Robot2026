0eea255882177eba9be4f62778da0e715c405022
Southern Tier Robotics Coder <team2053tigertronics@gmail.com>
2026-01-14 20:05:01 -0500

    new tuner constants

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "1b7184d258515708bda4100ffdfe0903d3a24450";
-  public static final String GIT_DATE = "2026-01-09 16:57:30 EST";
-  public static final String GIT_BRANCH = "add-vision";
-  public static final String BUILD_DATE = "2026-01-09 19:01:50 EST";
-  public static final long BUILD_UNIX_TIME = 1768003310847L;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "9d7c6a9199a362393d9bf0c19aa408703ab28905";
+  public static final String GIT_DATE = "2026-01-09 19:44:58 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-14 20:04:25 EST";
+  public static final long BUILD_UNIX_TIME = 1768439065686L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
