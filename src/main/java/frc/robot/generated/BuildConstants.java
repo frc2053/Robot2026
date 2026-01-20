ebe5171d89b5c30b7366af6e99bc81cd3d0c4f1e
Drew Williams <williams.r.drew@gmail.com>
2026-01-20 15:20:01 -0800

    updated to wpilib 2026.2.1

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "9d7c6a9199a362393d9bf0c19aa408703ab28905";
-  public static final String GIT_DATE = "2026-01-09 19:44:58 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 20:04:25 EST";
-  public static final long BUILD_UNIX_TIME = 1768439065686L;
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "b8f17c3b266f828011a6632f2829de8b38b94015";
+  public static final String GIT_DATE = "2026-01-14 20:05:07 EST";
+  public static final String GIT_BRANCH = "comp-chassis";
+  public static final String BUILD_DATE = "2026-01-20 18:18:57 EST";
+  public static final long BUILD_UNIX_TIME = 1768951137786L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
