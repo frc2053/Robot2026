5b9a524db00d6e2a3d178a4679a5314ad964fca2
Drew Williams <williams.r.drew@gmail.com>
2026-01-20 15:40:26 -0800

    changed to comp bot constants

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 16;
-  public static final String GIT_SHA = "b8f17c3b266f828011a6632f2829de8b38b94015";
-  public static final String GIT_DATE = "2026-01-14 20:05:07 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "3d049234e84c0e7b37d3f9f995a5954dba5ac395";
+  public static final String GIT_DATE = "2026-01-20 18:20:01 EST";
   public static final String GIT_BRANCH = "comp-chassis";
-  public static final String BUILD_DATE = "2026-01-20 18:18:57 EST";
-  public static final long BUILD_UNIX_TIME = 1768951137786L;
+  public static final String BUILD_DATE = "2026-01-20 18:35:54 EST";
+  public static final long BUILD_UNIX_TIME = 1768952154393L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
+import frc.robot.generated.CompBotTunerConstants.TunerSwerveDrivetrain;
 import java.util.Optional;
 import java.util.function.Supplier;
 
