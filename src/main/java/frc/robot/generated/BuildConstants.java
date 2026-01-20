079e4308e756de26cf2561d8fd224265205cc8b2
Drew Williams <williams.r.drew@gmail.com>
2026-01-20 15:50:37 -0800

    pathplanner added

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 17;
-  public static final String GIT_SHA = "3d049234e84c0e7b37d3f9f995a5954dba5ac395";
-  public static final String GIT_DATE = "2026-01-20 18:20:01 EST";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "80c1b26f6549b6f33947202ffa461329eaf1b51f";
+  public static final String GIT_DATE = "2026-01-20 18:40:26 EST";
   public static final String GIT_BRANCH = "comp-chassis";
-  public static final String BUILD_DATE = "2026-01-20 18:35:54 EST";
-  public static final long BUILD_UNIX_TIME = 1768952154393L;
+  public static final String BUILD_DATE = "2026-01-20 18:46:04 EST";
+  public static final long BUILD_UNIX_TIME = 1768952764633L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
