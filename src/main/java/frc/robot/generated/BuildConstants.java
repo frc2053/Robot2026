ef40b8f462201ef27806608c099fca87f56e57ab
Drew Williams <williams.r.drew@gmail.com>
2026-01-24 18:02:23 -0800

    added robot model

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 24;
-  public static final String GIT_SHA = "8f94e64d6aa593908bf412c0d2a09b0ab42a9b66";
-  public static final String GIT_DATE = "2026-01-23 14:38:42 EST";
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "fae074bc098a965360a1ca71a26f96a73d730278";
+  public static final String GIT_DATE = "2026-01-23 14:43:12 EST";
   public static final String GIT_BRANCH = "comp-chassis";
-  public static final String BUILD_DATE = "2026-01-23 14:42:46 EST";
-  public static final long BUILD_UNIX_TIME = 1769197366799L;
+  public static final String BUILD_DATE = "2026-01-24 20:54:00 EST";
+  public static final long BUILD_UNIX_TIME = 1769306040301L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
