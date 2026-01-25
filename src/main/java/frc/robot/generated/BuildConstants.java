e5334fcb96c8e55b9a545f7fc7c5fbf878d0a621
Drew Williams <williams.r.drew@gmail.com>
2026-01-24 21:32:10 -0800

    updated ascope

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "ffe1d64cc43175eea5e8b068696686bddf06f7b6";
-  public static final String GIT_DATE = "2026-01-24 22:26:17 EST";
+  public static final int GIT_REVISION = 32;
+  public static final String GIT_SHA = "864efcbd7f421db862f99127f1315ba62aae6587";
+  public static final String GIT_DATE = "2026-01-25 00:29:39 EST";
   public static final String GIT_BRANCH = "comp-chassis";
-  public static final String BUILD_DATE = "2026-01-25 00:29:26 EST";
-  public static final long BUILD_UNIX_TIME = 1769318966147L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-25 00:30:28 EST";
+  public static final long BUILD_UNIX_TIME = 1769319028188L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
