387bca56a1feb378820871fb468f8ec90ef6a4bc
Drew Williams <williams.r.drew@gmail.com>
2026-01-29 21:44:19 -0800

    updated camera pose based off cad

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 36;
-  public static final String GIT_SHA = "d29f12f7f59d66690d0f618b051042e87ebbf3de";
-  public static final String GIT_DATE = "2026-01-29 22:46:15 EST";
+  public static final int GIT_REVISION = 38;
+  public static final String GIT_SHA = "9154f877bd12af52278051475f16989839273bb4";
+  public static final String GIT_DATE = "2026-01-30 00:31:38 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-29 22:47:02 EST";
-  public static final long BUILD_UNIX_TIME = 1769744822085L;
+  public static final String BUILD_DATE = "2026-01-30 00:40:50 EST";
+  public static final long BUILD_UNIX_TIME = 1769751650678L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
