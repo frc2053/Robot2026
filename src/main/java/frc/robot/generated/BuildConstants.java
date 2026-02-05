2fc92486117981a239471ea22e000c5300141c18
Drew Williams <williams.r.drew@gmail.com>
2026-02-05 01:17:07 -0800

    Addedkicker (#4)
    * Addedkicker
    
    * fix
    
    * formatting
    
    * added kicker stuff
    
    ---------
    
    Co-authored-by: Southern Tier Robotics Coder <team2053tigertronics@gmail.com>

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 40;
-  public static final String GIT_SHA = "5cc622b08ffc433728bf83047e1aab2b62a094b3";
-  public static final String GIT_DATE = "2026-01-30 02:10:23 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-30 02:13:08 EST";
-  public static final long BUILD_UNIX_TIME = 1769757188519L;
+  public static final int GIT_REVISION = 44;
+  public static final String GIT_SHA = "a421fdde4e4ed3a0a8742d5172216408bca74cb0";
+  public static final String GIT_DATE = "2026-02-04 22:58:53 EST";
+  public static final String GIT_BRANCH = "KickerAdding";
+  public static final String BUILD_DATE = "2026-02-04 23:17:30 EST";
+  public static final long BUILD_UNIX_TIME = 1770265050088L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
