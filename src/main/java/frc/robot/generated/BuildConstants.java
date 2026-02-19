3215387426ce5eecf5673514aa9995bcd56318bf
Drew Williams <williams.r.drew@gmail.com>
2026-02-19 08:43:49 -0800

    Newversion (#5)
    * updated_Gear_Ratio
    
    * edit
    
    ---------
    
    Co-authored-by: Southern Tier Robotics Coder <team2053tigertronics@gmail.com>

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 44;
-  public static final String GIT_SHA = "a421fdde4e4ed3a0a8742d5172216408bca74cb0";
-  public static final String GIT_DATE = "2026-02-04 22:58:53 EST";
-  public static final String GIT_BRANCH = "KickerAdding";
-  public static final String BUILD_DATE = "2026-02-04 23:17:30 EST";
-  public static final long BUILD_UNIX_TIME = 1770265050088L;
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "f412ab5b2760269a05b9a06e60d3cb08a5902c9e";
+  public static final String GIT_DATE = "2026-02-18 18:13:09 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-02-18 19:49:34 EST";
+  public static final long BUILD_UNIX_TIME = 1771462174802L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
