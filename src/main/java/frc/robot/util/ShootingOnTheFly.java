6f368555456a98d641ab7a6f14732454b010dcfd
Drew Williams <williams.r.drew@gmail.com>
2026-03-02 21:09:00 -0800

    cleaning up math

--- src/main/java/frc/robot/util/ShootingOnTheFly.java
+++ src/main/java/frc/robot/util/ShootingOnTheFly.java
@@ -5,14 +5,10 @@
 package frc.robot.util;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StructPublisher;
 import frc.robot.Constants.FuelConstants;
 
 /**
