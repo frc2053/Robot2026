fefec80dbd8b8b005cc44005507ced897b338077
Drew Williams <williams.r.drew@gmail.com>
2026-03-02 21:37:59 -0800

    made choreo and pathplanner account for intake

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 71;
-  public static final String GIT_SHA = "5a3646f6ad3b402b796767fdd64b2c62a1ca3d1c";
-  public static final String GIT_DATE = "2026-03-02 23:54:46 EST";
+  public static final int GIT_REVISION = 73;
+  public static final String GIT_SHA = "6defdd18da9477ba4fbf2daec7084ba604996826";
+  public static final String GIT_DATE = "2026-03-03 00:20:34 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-03 00:06:08 EST";
-  public static final long BUILD_UNIX_TIME = 1772514368845L;
+  public static final String BUILD_DATE = "2026-03-03 00:36:51 EST";
+  public static final long BUILD_UNIX_TIME = 1772516211911L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
