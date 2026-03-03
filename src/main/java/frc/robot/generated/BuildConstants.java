817490889a8c64050a02c64b616f6208be058b72
Southern Tier Robotics Coder <team2053tigertronics@gmail.com>
2026-03-02 19:28:50 -0500

    Changed Current Value and kP value

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 68;
-  public static final String GIT_SHA = "53cd3c44adf0d3aab41f34e03ff0e5f7ffdbea17";
-  public static final String GIT_DATE = "2026-02-21 23:05:36 EST";
+  public static final int GIT_REVISION = 69;
+  public static final String GIT_SHA = "b9c46d3ddc049eaa186479f08e74e68afcebfcaf";
+  public static final String GIT_DATE = "2026-03-02 18:12:25 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-28 15:08:15 EST";
-  public static final long BUILD_UNIX_TIME = 1772309295296L;
+  public static final String BUILD_DATE = "2026-03-02 19:17:29 EST";
+  public static final long BUILD_UNIX_TIME = 1772497049618L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
