81d4c278c12f2add6c1610f76cd10361b252a9da
Drew Williams <williams.r.drew@gmail.com>
2026-03-08 18:55:54 -0700

    update vendor libs

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 88;
-  public static final String GIT_SHA = "2087fe368bf8a79d76886bcd22860d607d2e1324";
-  public static final String GIT_DATE = "2026-03-07 16:45:39 EST";
+  public static final int GIT_REVISION = 89;
+  public static final String GIT_SHA = "040735d349b04827df79032717f36a7604b83ea4";
+  public static final String GIT_DATE = "2026-03-07 22:38:36 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-07 22:34:22 EST";
-  public static final long BUILD_UNIX_TIME = 1772940862044L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-08 21:55:42 EDT";
+  public static final long BUILD_UNIX_TIME = 1773021342668L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
