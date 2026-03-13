7c4c5e0fd44a1e23a741ce07f7e01287debdc168
Southern Tier Robotics Coder <team2053tigertronics@gmail.com>
2026-03-13 19:09:15 -0400

    newkValues

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 96;
-  public static final String GIT_SHA = "6a57a910b14b24b1f7208c843e5516ef098825bb";
-  public static final String GIT_DATE = "2026-03-10 14:40:48 EDT";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "25dc2a1d706ad6b9a813af1095c42ddbd1669943";
+  public static final String GIT_DATE = "2026-03-13 12:04:01 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-10 18:23:51 EDT";
-  public static final long BUILD_UNIX_TIME = 1773181431463L;
+  public static final String BUILD_DATE = "2026-03-13 19:02:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1773442977665L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
